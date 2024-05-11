4050430634b1fbc30f3ca6209551f36f32708746
ahmadnial <ahmadhky@gmail.com>
2024-05-12 00:41:04 +0700

    perubahan tipe tarif

--- resources/views/Pages/penjualan.blade.php
+++ resources/views/Pages/penjualan.blade.php
@@ -164,8 +164,8 @@
                                 <select class="form-control" onchange="getTipeTarif()" id="tp_tipe_tarif"
                                     style="width: 100%;" name="tp_tipe_tarif">
                                     <option value="">--Select--</option>
-                                    <option value="Reguler">Reguler</option>
-                                    <option value="Resep">Resep</option>
+                                    <option value="Reguler">Customer</option>
+                                    <option value="Resep">Grosir/Toko</option>
                                     <option value="Nakes">Nakes</option>
                                 </select>
                                 <div class="invalid-feedback">Please..dont let me blank</div>
@@ -414,8 +414,8 @@
                                 <select class="tp_tipe_tarife form-control" onchange="getTipeTarifE()"
                                     id="tp_tipe_tarife" style="width: 100%;" name="tp_tipe_tarife">
                                     <option value="">--Select--</option>
-                                    <option value="Reguler">Reguler</option>
-                                    <option value="Resep">Resep</option>
+                                    <option value="Reguler">Customer</option>
+                                    <option value="Resep">Grosir/Toko</option>
                                     <option value="Nakes">Nakes</option>
                                 </select>
                             </div>
@@ -766,7 +766,7 @@
                 var tes = $('#tp_tipe_tarif').val();
 
                 if (tes == 'Reguler') {
-                    toastr.info('Harga Reguler Selected!', {
+                    toastr.info('Harga Customer Selected!', {
                         timeOut: 600,
                         // preventDuplicates: true,
                         positionClass: 'toast-top-right',
@@ -813,7 +813,7 @@
 
                 } else if (tes == 'Resep') {
                     $("#getListObatx").empty();
-                    toastr.info('Harga Resep Selected!', {
+                    toastr.info('Harga Grosir/Toko Selected!', {
                         timeOut: 600,
                         // preventDuplicates: true,
                         positionClass: 'toast-top-right',
@@ -920,7 +920,7 @@
                 var tes = $('#tp_tipe_tarife').val();
 
                 if (tes == 'Reguler') {
-                    toastr.info('Harga Reguler Selected!', {
+                    toastr.info('Harga Customer Selected!', {
                         timeOut: 600,
                         // preventDuplicates: true,
                         positionClass: 'toast-top-right',
@@ -968,7 +968,7 @@
 
                 } else if (tes == 'Resep') {
                     $("#getListObatx").empty();
-                    toastr.info('Harga Resep Selected!', {
+                    toastr.info('Harga Grosir/Toko Selected!', {
                         timeOut: 600,
                         // preventDuplicates: true,
                         positionClass: 'toast-top-right',
